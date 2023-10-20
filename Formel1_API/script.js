8fd943c6e9e7e9dcbddf5b271051e81689a82cc9
andreas riedmann <andreas@riedmann.rocks>
2023-10-20 14:23:41 +0200

    async problem solved

--- Formel1_API/script.js
+++ Formel1_API/script.js
@@ -1,50 +1,49 @@
 document.getElementById("test2").addEventListener("click", function () {
-    fetch("http://ergast.com/api/f1/2023/circuits.json").then((result) => {
-        result.json().then((data => {
-            //console.log(data)
-            FillTable(data.MRData.CircuitTable.Circuits)
-        }))
-    })
-})
+  fetch("http://ergast.com/api/f1/2023/circuits.json").then((result) => {
+    result.json().then((data) => {
+      //console.log(data)
+      FillTable(data.MRData.CircuitTable.Circuits);
+    });
+  });
+});
 
 var circuitDataFinal = [];
 
-let FillTable = (data) => {
+let fillNameOfCurcuit = async (data) => {
+  let index = 0;
+  for (const element of data) {
+    let result = await fetch(
+      `http://ergast.com/api/f1/circuits/${data[index].circuitId}/results.json`
+    );
+    let resultAsJson = await result.json();
+    console.log("index:" + index, resultAsJson);
+    
+    let ShowBoxBody = document.getElementById("ShowBoxBody");
+    const ShowBoxes = document.createElement("div");
+    ShowBoxes.classList.add("ShowBoxes", `ShowBoxes${index}`);
+    ShowBoxes.id = `ShowBoxes${index}`;
+    ShowBoxBody.appendChild(ShowBoxes);
+    ShowBoxes.innerHTML = "Circuit Name:" + data[index].circuitName;
+    index++;
+  }
+};
 
-    data.forEach((element, index) => {
-        fetch(`http://ergast.com/api/f1/circuits/${data[index].circuitId}/results.json`).then((result) => {
-            result.json().then((circuitData => {
-                console.log(circuitData)
-                circuitDataFinal.push(circuitData.MRData.RaceTable.Races);
-                console.log(circuitDataFinal);
-            }))
-        })
-        let ShowBoxBody = document.getElementById("ShowBoxBody");
-        const ShowBoxes = document.createElement("div");
-        ShowBoxes.classList.add("ShowBoxes", `ShowBoxes${index}`);
-        ShowBoxes.id = `ShowBoxes${index}`
-        ShowBoxBody.appendChild(ShowBoxes)
-        ShowBoxes.innerHTML = "Circuit Name:" + data[index].circuitName;
-    });
-    FillTable2(circuitDataFinal);
+let FillTable = async (data) => {
+ 
+  let res = await fillNameOfCurcuit(data);
+ 
+  FillTable2(circuitDataFinal);
 };
 
 let FillTable2 = (circuitDataFinal) => {
-    console.log("inside");
+  console.log("inside");
 
-    circuitDataFinal.forEach((element, index) => {
-        let ShowBoxInfos = document.getElementById(`ShowBoxes${index}`);
-        const Infos = document.createElement("ul");
-        Infos.classList.add("ShowBoxInfos", `ShowBoxInfos${index}`);
-        Infos.id = `ShowBoxInfos${index}`;
-        Infos.innerHTML = "Race Infos:" //+ circuitData.season;
-        ShowBoxInfos.appendChild(Infos)
-    });
-
-
-
-
-
-}
-
-
+  circuitDataFinal.forEach((element, index) => {
+    let ShowBoxInfos = document.getElementById(`ShowBoxes${index}`);
+    const Infos = document.createElement("ul");
+    Infos.classList.add("ShowBoxInfos", `ShowBoxInfos${index}`);
+    Infos.id = `ShowBoxInfos${index}`;
+    Infos.innerHTML = "Race Infos:"; //+ circuitData.season;
+    ShowBoxInfos.appendChild(Infos);
+  });
+};
